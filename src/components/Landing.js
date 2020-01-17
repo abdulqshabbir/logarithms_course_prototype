ed4b18ffdd7f6589f42addb55b4d9902d0cff2b9
Abdul Shabbir <abdulqshabbir@gmail.com>
2020-01-17 18:12:50 +0800

    Menu bar with underline effect created.  The underline effect is not properly posiitoned and needs to be fixed.

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,24 +1,17 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import "semantic-ui-css";
 import "./Landing.css";
 import React from "react";
 
 const MenuBar = () => {
   return (
-    <div>
-      <div class="ui pointing menu">
-        <a class="active item" href="localhost:3000">
-          Home
-        </a>
-        <a class="item" href="localhost:3000">
-          Logarithm Problems
-        </a>
-        <a class="item" href="localhost:3000">
-          Contact Us
-        </a>
-      </div>
-      <h1>This is a header tag. </h1>
-      <h3>This is a subheading. </h3>
-      <p>This is a paragraph text. </p>
+    <div class="menu-bar">
+      <p class="links">
+        <a href="#"> Home </a>
+        <a href="#"> Logarithm Problems </a>
+        <a href="#"> Video Solutions </a>
+        <a href="#"> Contact Us </a>
+      </p>
     </div>
   );
 };
