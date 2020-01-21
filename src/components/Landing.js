4934fd4928b114c657965e845bfbc952cb7ec778
Abdul Shabbir <abdulqshabbir@gmail.com>
2020-01-21 18:04:39 -0500

    Created logo for web application in svg format.

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,17 +1,24 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import "semantic-ui-css";
+import { Container } from "semantic-ui-react";
 import "./Landing.css";
+import Logo from "./Logo";
 import React from "react";
 
 const MenuBar = () => {
   return (
-    <div className="menu-bar">
-      <p className="links">
-        <a href="#"> Home </a>
-        <a href="#"> Logarithm Problems </a>
-        <a href="#"> Video Solutions </a>
-        <a href="#"> Contact Us </a>
-      </p>
+    <div>
+      <div className="menu-bar">
+        <Logo />
+        <p className="links">
+          <a href="#"> Home </a>
+          <a href="#"> Logarithm Problems </a>
+          <a href="#"> Video Solutions </a>
+          <a href="#"> Contact Us </a>
+        </p>
+      </div>
+      <div>
+        <Container></Container>
+      </div>
     </div>
   );
 };
---
+++ src/components/Logo.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <svg id="logo" width="100" height="100">
+      <rect
+        x="5px"
+        y="5px"
+        width="93%"
+        height="93%"
+        fill="none"
+        stroke="black"
+        strokeWidth="2%"
+      ></rect>
+      <line
+        x1="50%"
+        y1="5%"
+        x2="50%"
+        y2="98%"
+        stroke="black"
+        strokeWidth="2%"
+      />
+      <line
+        x1="50%"
+        y1="50%"
+        x2="98%"
+        y2="50%"
+        stroke="black"
+        strokeWidth="2%"
+      />
+      <line
+        x1="73%"
+        y1="50%"
+        x2="73%"
+        y2="98%"
+        stroke="black"
+        strokeWidth="2%"
+      />
+      <line
+        y1="73%"
+        x1="73%"
+        y2="73%"
+        x2="50%"
+        stroke="black"
+        strokeWidth="2%"
+      />
+      <line
+        y1="73%"
+        x1="61.5%"
+        y2="50%"
+        x2="61.5%"
+        stroke="black"
+        strokeWidth="2%"
+      />
+    </svg>
+  );
+};
+
+export default Logo;
