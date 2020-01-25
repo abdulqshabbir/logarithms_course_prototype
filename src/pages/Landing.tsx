e4c62cc1e618be05b2f6838f0008f1ed4827d187
Abdul Shabbir <abdulqshabbir@gmail.com>
2020-01-25 13:12:36 -0500

    Added typescript to project.

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,42 +1,42 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import * as React from "react";
 import "./Landing.css";
 import Logo from "../components/Logo";
-import React from "react";
 import { Icon } from "semantic-ui-react";
 
-const MenuBar = () => {
+const MenuBar: React.FC = () => {
   return (
     <div className="container">
       <nav className="nav">
         <div className="nav-logo">
           <Logo />
         </div>
-        <div className="nav-item">Home</div>
-        <div className="nav-item">HL Problems</div>
-        <div className="nav-item">Solutions</div>
-        <div className="nav-item">Contact Us</div>
+        <div className="nav-item"> Home </div>
+        <div className="nav-item"> HL Problems </div>
+        <div className="nav-item"> Solutions </div>
+        <div className="nav-item"> Contact Us </div>
       </nav>
-      <div className="hero">Welcome To HL Academy.</div>
+      <div className="hero"> Welcome To HL Academy.</div>
       <main className="content-main">
-        <h1>What do we do?</h1>
+        <h1>What do we do? </h1>
         <p>
           HL Academy provides IB Math HL questions to help students prepare for
-          their upcoming examinations. Each question is representative of the
-          depth, difficulty and rigor of actual IB examination questions. Every
+          their upcoming examinations.Each question is representative of the
+          depth, difficulty and rigor of actual IB examination questions.Every
           question on our site is paired with a video explanation for the
-          problem-solving strategy needed to solve questions of a similar
+          problem - solving strategy needed to solve questions of a similar
           nature.
         </p>
       </main>
       <div className="content-secondary">
         <div className="content">
-          <h1>Clear explanations</h1>
+          <h1>Clear explanations </h1>
           <p>
             We provide clear, concise explanations on foundational concepts to
             get you up and running.
           </p>
           <div className="icon">
-            <Icon bordered size="huge" name="black bordered inverted pencil" />
+            <Icon bordered size="huge" color="black" inverted name="pencil" />
           </div>
         </div>
         <div className="content">
@@ -46,11 +46,7 @@ const MenuBar = () => {
             unfamiliar problems that require an novel approach.
           </p>
           <div className="icon">
-            <Icon
-              bordered
-              size="huge"
-              name="black bordered inverted question"
-            />
+            <Icon bordered size="huge" color="black" inverted name="question" />
           </div>
         </div>
         <div className="content">
@@ -63,13 +59,15 @@ const MenuBar = () => {
           <div className="icon">
             <Icon
               bordered
+              inverted
+              color="black"
               size="huge"
-              name="black bordered inverted video play"
+              name="video play"
             />
           </div>
         </div>
       </div>
-      <footer className="footer">&copy; 2020 HL Academy</footer>
+      <footer className="footer">& copy; 2020 HL Academy </footer>
     </div>
   );
 };
